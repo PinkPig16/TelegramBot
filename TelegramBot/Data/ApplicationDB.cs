b2e68a19a02669d162a805709f7bf61d67731dad
PinkPig16 <808nosky808@gmail.com>
2024-06-23 13:05:11 +0300

    Add project files.

---
+++ TelegramBot/Data/ApplicationDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TelegramBot.Data
+{
+    public class ApplicationDB : DbContext
+    {
+        public ApplicationDB(DbContextOptions<ApplicationDB> options) : base(options) 
+        {
+
+        }
+    }
+}
